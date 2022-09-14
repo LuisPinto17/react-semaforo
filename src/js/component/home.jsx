20fa2097385387171fdadb5aa1dc68bbaca03e77
LuisPinto17 <lapinto.17@est.ucab.edu.ve>
2022-09-14 23:39:41 +0000

    casi listo

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,32 +1,53 @@
-import React from "react";
+import React , {useState} from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
+	const [ colorLuz, setColorLuz] = useState(undefined);
+	const handleOn = (cambioLuz)=>{
+		if (colorLuz == cambioLuz) {
+			setColorLuz("ninguna")
+		}
+		else 
+		{
+			setColorLuz(cambioLuz)
+		}
+	}
+
+	const handleChange = ()=>{
+		let colors = ["red","yellow","green"];
+		let i = 0;
+		setInterval(()=>{
+			setColorLuz(colors[i%3]);
+			i++;
+		},1000)
+	}
+
 	return (
 		<div className="main-contain"> 
 			<div className="vara"></div>
 			<div className="d-flex justify-content-center m-auto"> 
 				<div className="card rounded-3" style={{width: "200px", height: "420px",backgroundColor: "black"}}>
 					<div className="d-flex justify-content-center mt-3">
-						<div className="luz rounded-circle" style={{width: "120px", height: "120px",backgroundColor: "red"}}>
+						<div className={colorLuz == "red" ? "red rounded-circle luz" : "red rounded-circle"} onClick={()=>{handleOn("red")}} style={{width: "120px", height: "120px"}}>
 						
 						</div>
 					</div>
 					<div className="d-flex justify-content-center mt-3">
-						<div className="luz rounded-circle" style={{width: "120px", height: "120px",backgroundColor: "yellow"}}>
+					<div className={colorLuz == "yellow" ? "yellow rounded-circle luz" : "yellow rounded-circle"} onClick={()=>{handleOn("yellow")}} style={{width: "120px", height: "120px"}}>
 						
 						</div>
 					</div>
 					<div className="d-flex justify-content-center mt-3">
-						<div className="luz rounded-circle" style={{width: "120px", height: "120px",backgroundColor: "green"}}>
+					<div className={colorLuz == "green" ? "green rounded-circle luz" : "green rounded-circle"} onClick={()=>{handleOn("green")}} style={{width: "120px", height: "120px"}}>
 						
 						</div>
 					</div>
 				</div>
 			</div>
+		<button onClick={() => handleChange()}>Test</button>
 		</div>
 
 	);
