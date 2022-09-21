cbd68e850865559f6889e6c4343dd26103fb9490
LuisPinto17 <lapinto.17@est.ucab.edu.ve>
2022-09-21 22:00:33 +0000

    falta color extra

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -3,16 +3,18 @@ import React , {useState} from "react";
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
+
 //create your first component
 const Home = () => {
-	const [ colorLuz, setColorLuz] = useState(undefined);
-	const handleOn = (cambioLuz)=>{
-		if (colorLuz == cambioLuz) {
-			setColorLuz("ninguna")
+	const [ colorLuzActual, setColorLuzActual] = useState(undefined);
+	
+	const handleOn = (cambioLuz)=>{ 
+		if (colorLuzActual == cambioLuz) {
+			setColorLuzActual("apagate")
 		}
 		else 
 		{
-			setColorLuz(cambioLuz)
+			setColorLuzActual(cambioLuz)
 		}
 	}
 
@@ -20,7 +22,7 @@ const Home = () => {
 		let colors = ["red","yellow","green"];
 		let i = 0;
 		setInterval(()=>{
-			setColorLuz(colors[i%3]);
+			setColorLuzActual(colors[i%3]);
 			i++;
 		},1000)
 	}
@@ -31,23 +33,26 @@ const Home = () => {
 			<div className="d-flex justify-content-center m-auto"> 
 				<div className="card rounded-3" style={{width: "200px", height: "420px",backgroundColor: "black"}}>
 					<div className="d-flex justify-content-center mt-3">
-						<div className={colorLuz == "red" ? "red rounded-circle luz" : "red rounded-circle"} onClick={()=>{handleOn("red")}} style={{width: "120px", height: "120px"}}>
+						<div className={colorLuzActual == "red" ? "red rounded-circle luz_roja" : "red rounded-circle opacity-50"} onClick={()=>{handleOn("red")}} style={{width: "120px", height: "120px"}}>
 						
 						</div>
 					</div>
 					<div className="d-flex justify-content-center mt-3">
-					<div className={colorLuz == "yellow" ? "yellow rounded-circle luz" : "yellow rounded-circle"} onClick={()=>{handleOn("yellow")}} style={{width: "120px", height: "120px"}}>
+					<div className={colorLuzActual == "yellow" ? "yellow rounded-circle luz" : "yellow rounded-circle opacity-50"} onClick={()=>{handleOn("yellow")}} style={{width: "120px", height: "120px"}}>
 						
 						</div>
 					</div>
 					<div className="d-flex justify-content-center mt-3">
-					<div className={colorLuz == "green" ? "green rounded-circle luz" : "green rounded-circle"} onClick={()=>{handleOn("green")}} style={{width: "120px", height: "120px"}}>
+					<div className={colorLuzActual == "green" ? "green rounded-circle luz_verde" : "green rounded-circle opacity-50"} onClick={()=>{handleOn("green")}} style={{width: "120px", height: "120px"}}>
 						
 						</div>
 					</div>
 				</div>
 			</div>
-		<button onClick={() => handleChange()}>Test</button>
+		<div className="text-center">
+			<button onClick={() => handleChange()}>Ciclar</button>
+			{/* <button onClick={() => handleChange()}>Parar</button> */}
+		</div>
 		</div>
 
 	);
